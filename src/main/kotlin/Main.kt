abc7d9e4d922d072019501250d7203e037eedd6d
phamtanphat <phatdroid94@gmail.com>
2024-01-15 19:33:23 +0700

    Kieu du lieu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,21 @@ fun main() {
     // name = "Phạm Tấn Phát Dạy Android"
 
     // val: sử dụng cho biến không được gán lại giá trị (immutable)
-    val phone: String = "0352408366"
-    phone = "abc"
+    // val phone: String = "0352408366"
+    // phone = "abc"
 
     // Phương thức in
     // print(name)
 
+    // 2: Kiểu dữ liệu
+    // Basic type: Kiểu dữ liệu cơ bản
+    // Reference type: Kiêu dữ liệu vùng nhớ
 
+    var name: String = "Phạm Tấn Phát"
+    var age: Int = 29
+    var height: Float = 1.75f
+    var weight: Double = 70.5
+    var isMale: Boolean = true
+
+    // Quy tắc đặt tên biến
 }
