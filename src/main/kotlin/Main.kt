7fb96a093e5f7843f695a28fe52a280593ba259c
phamtanphat <phatdroid94@gmail.com>
2024-01-15 19:08:24 +0700

    Khai bao bien

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // 1: Khai báo biến
+    // Từ khoá khai báo biến: var , val
+
+    // var: sử dụng cho biến có thể thay đổi giá trị (mutable)
+    // var name: String = "Phạm Tấn Phát"
+    // name = "Phạm Tấn Phát Dạy Android"
+
+    // val: sử dụng cho biến không được gán lại giá trị (immutable)
+    val phone: String = "0352408366"
+    phone = "abc"
+
+    // Phương thức in
+    // print(name)
+
+
+}
