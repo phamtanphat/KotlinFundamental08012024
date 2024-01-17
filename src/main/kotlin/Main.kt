9ffe030cb704b732f95b7520205ef88faac5fdfc
phamtanphat <phatdroid94@gmail.com>
2024-01-17 18:11:50 +0700

    refactor comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,30 @@
 fun main() {
-    // 1: Khai báo biến
-    // Từ khoá khai báo biến: var , val
+    /*
+         1: Khai báo biến
+         Từ khoá khai báo biến: var , val
 
-    // var: sử dụng cho biến có thể thay đổi giá trị (mutable)
-    // var name: String = "Phạm Tấn Phát"
-    // name = "Phạm Tấn Phát Dạy Android"
+         var: sử dụng cho biến có thể thay đổi giá trị (mutable)
+         var name: String = "Phạm Tấn Phát"
+         name = "Phạm Tấn Phát Dạy Android"
 
-    // val: sử dụng cho biến không được gán lại giá trị (immutable)
-    // val phone: String = "0352408366"
-    // phone = "abc"
+         val: sử dụng cho biến không được gán lại giá trị (immutable)
+         val phone: String = "0352408366"
+         phone = "abc"
 
-    // Phương thức in
-    // print(name)
+         Phương thức in
+         print(name)
 
-    // 2: Kiểu dữ liệu
-    // Basic type: Kiểu dữ liệu cơ bản
-    // Reference type: Kiêu dữ liệu vùng nhớ
+        2: Kiểu dữ liệu
+        Basic type: Kiểu dữ liệu cơ bản
+        Reference type: Kiêu dữ liệu vùng nhớ
 
-    var name: String = "Phạm Tấn Phát"
-    var age: Int = 29
-    var height: Float = 1.75f
-    var weight: Double = 70.5
-    var isMale: Boolean = true
+        var name: String = "Phạm Tấn Phát"
+        var age: Int = 29
+        var height: Float = 1.75f
+        var weight: Double = 70.5
+        var isMale: Boolean = true
 
-    // Quy tắc đặt tên biến
+        Quy tắc đặt tên biến
+     */
+
 }
